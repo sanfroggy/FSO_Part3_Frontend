b2c43d67508af6c4d8cad72aab3fd01d5730026b
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-09-15 04:44:14 +0300

    Part 3.11

--- src/services/Persons.js
+++ src/services/Persons.js
@@ -2,12 +2,12 @@ import axios from 'axios'
 
 const baseUrl = '/api/persons'
 
-//Creating a function to return all contacts from JSON server.
+//Creating a function to return all contacts from Node backend.
 const getAll = () => {
     const request = axios.get(baseUrl)
     return request.then(response => response.data)
 }
-//Creating a function to add a new contact to JSON server.
+//Creating a function to add a new contact to Node backend.
 const createContact = newContact => {
     const request = axios.post(baseUrl, newContact)
     return request.then(response => response.data)
@@ -20,7 +20,7 @@ const updateContact = (contactId, newObject) => {
     return request.then(response => response.data)
 } */
 
-//Creating a function to delete a contact from JSON server.
+//Creating a function to delete a contact from Node backend.
 const deleteContact = (contactId) => {
 
     const request = axios.delete(`${baseUrl}/${contactId}`)
