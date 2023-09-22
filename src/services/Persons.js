8bec3e712d8ac3922dc804e703e29d8815b57830
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-09-22 11:54:27 +0300

    Edited part 3.20

--- src/services/Persons.js
+++ src/services/Persons.js
@@ -6,28 +6,29 @@ const baseUrl = '/api/persons'
 /*Creating a function to return all contacts from MongoDB database
 through Node backend. */
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
 }
 /*Creating a function to add a new contact to MongoDB database
 through Node backend. */
 const createContact = newContact => {
-  const request = axios.post(baseUrl, newContact)
-  return request.then(response => response.data)
+    const request = axios.post(baseUrl, newContact)
+    return request.then(response => response.data)
 }
 
 /*Creating a function to provide an already existing contact
 with a new number on MongoDB database through Node backend. */
 const updateContact = (contactId, newObject) => {
-  const request = axios.put(`${baseUrl}/${contactId}`, newObject)
-  return request.then(response => response.data)
+    const request = axios.put(`${baseUrl}/${contactId}`, newObject)
+    return request.then(response => response.data)
 }
 
 /*Creating a function to delete a contact from MongoDB database
 through Node backend. */
 const deleteContact = (contactId) => {
-  const request = axios.delete(`${baseUrl}/${contactId}`)
-  return request.then(response => response.data)
+    const request = axios.delete(`${baseUrl}/${contactId}`)
+    return request.then(response => response.data)
 }
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { getAll, createContact, updateContact, deleteContact }
